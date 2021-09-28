0b03c564ee9536b33500e7e90d6bcc3660d76612
AzureAD\AmitNautiyal <amit.nautiyal@onlinerepublic.com>
2021-09-28 14:08:21 +1300

    use JS fetch to get json data and console log the output

--- app/script.js
+++ app/script.js
@@ -1,1 +1,18 @@
-alert("Hello Envato");
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('https://envato.github.io/front-end-coding-test/test.json').then(function (response) {
+    // The API call was successful!
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }).then(function (data) {
+        // This is the JSON from our response
+        console.log(data);
+        console.log('data------', data)
+        
+    }).catch(function (err) {
+        // There was an error
+        console.warn('Something went wrong.', err);
+    });
+});
