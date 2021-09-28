184ae07106b53c21d1083955a9a2d3ca0efc39d1
AzureAD\AmitNautiyal <amit.nautiyal@onlinerepublic.com>
2021-09-28 14:21:21 +1300

    build HTML for data received

--- app/script.js
+++ app/script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+    let root = document.querySelector('#root');
+
     fetch('https://envato.github.io/front-end-coding-test/test.json').then(function (response) {
     // The API call was successful!
         if (response.ok) {
@@ -9,10 +11,38 @@ document.addEventListener("DOMContentLoaded", function() {
     }).then(function (data) {
         // This is the JSON from our response
         console.log(data);
-        console.log('data------', data)
+        console.log('data------', data);
+        root.innerHTML = buildList(data);
         
     }).catch(function (err) {
         // There was an error
         console.warn('Something went wrong.', err);
     });
 });
+
+function buildList(items) {
+  
+    let html = ''
+    
+    for (item of items.items) {
+     
+      // start a section element for each item
+      
+      
+      // create a <div> for each key-value pair
+      for (key in item) {
+        html += '<div class="card">';
+        html += `<div class="card__img"><img src='${item.cover_image[3].url}' /></div>`
+        html += `<div class="card__title">${item.title}</div>`
+        html += `<div class="card__author">by ${item.author}</div>`
+        html += `<div class="card__footer">`
+        html += `<div class="card__ratings">${item.rating}</div>`
+        html += `<div class="card__action">Remove</div>`
+        html += `</div>`
+        html += '</div>';
+      }
+      // close off the section
+    }
+    // return the html
+    return html;
+  }
