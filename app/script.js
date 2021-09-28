c5ac62f44dc4a5e45f869bfc6aba50eda3e457ac
AzureAD\AmitNautiyal <amit.nautiyal@onlinerepublic.com>
2021-09-28 14:33:00 +1300

    rounded down ratings to the nearest whole number

--- app/script.js
+++ app/script.js
@@ -21,28 +21,19 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function buildList(items) {
-  
     let html = ''
-    
     for (item of items.items) {
-     
-      // start a section element for each item
-      
-      
-      // create a <div> for each key-value pair
       for (key in item) {
         html += '<div class="card">';
         html += `<div class="card__img"><img src='${item.cover_image[3].url}' /></div>`
         html += `<div class="card__title">${item.title}</div>`
         html += `<div class="card__author">by ${item.author}</div>`
         html += `<div class="card__footer">`
-        html += `<div class="card__ratings">${item.rating}</div>`
+        html += `<div class="card__ratings">${Math.floor(item.rating)}</div>`
         html += `<div class="card__action">Remove</div>`
         html += `</div>`
         html += '</div>';
       }
-      // close off the section
     }
-    // return the html
     return html;
   }
